dc6903cf47bd1107772f1967197e66aa7d87cecc
Alhaitham Al Jabri <jabri.dev@gmail.com>
2023-01-03 21:56:41 +0400

    Update .doom.d/config.el

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -4,9 +4,25 @@
 (setq user-full-name "Alhaitham Al Jabri"
       user-mail-address "jabri.dev@gmail.com")
 
-(setq doom-theme 'doom-dark+)
+;; doom theme
+(setq doom-theme 'leuven)
 
+;; doom font
 (setq display-line-numbers-type t)
 
-;; leetcode
+;; leetcode config
 (setq leetcode-prefer-language "golang")
+
+;; copilot mode config
+(use-package! copilot
+  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
+         ("C-<tab>" . 'copilot-accept-completion-by-word)
+         :map copilot-completion-map
+         ("<tab>" . 'copilot-accept-completion)
+         ("TAB" . 'copilot-accept-completion)
+         ("C-g" . #'copilot-clear-overlay)
+         ("C-n" . #'copilot-next-completion)
+         ("C-p" . #'copilot-previous-completion)))
+
+;; docstring warning fix
+(setq byte-compile-warnings '(not docstrings))
